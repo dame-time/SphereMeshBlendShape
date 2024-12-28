2c61e0fd472a5a72927de36b2aa8956e4fbea74f
dame-time <davide.paolillo.uni@gmail.com>
2024-12-28 12:39:47 +0100

    Removed main, fixed readme and typo in filenames

--- main.cpp
+++
@@ -1,9 +0,0 @@
-#include "sphere_mesh.h"
-
-using namespace SM;
-
-int main() {
-	SphereMesh s;
-	s.loadFromFile("/Users/davidepaollilo/Workspaces/C++/SphereMeshBlendShape/smaug.txt");
-	return 0;
-}
